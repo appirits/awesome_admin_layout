5c8eded29b12a56809e61d561fec6ec2edc50f8b
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-06-03 17:10:49 +0900

    Fix a problem that fail the access of first time

--- lib/awesome_admin_layout/script.rb
+++ lib/awesome_admin_layout/script.rb
@@ -4,15 +4,32 @@ require 'awesome_admin_layout/navigation/flex_divider'
 
 module AwesomeAdminLayout
   class Script
+    attr_accessor :nagigations
+
+    def initialize
+      @nagigations = {}
+    end
+
     def navigation(key = :default, &block)
-      navigation = Navigation.new(key)
-      navigation.instance_eval(&block)
+      nagigations[key] = block
+      Navigation.new(key)
     end
 
     def to_s
+      navigation_script_eval if nagigations.any?
       Navigation.find(:default)
     end
 
     alias_method :to_html, :to_s
+
+    private
+
+    def navigation_script_eval
+      Navigation.each_pair do |key, navigation|
+        code = nagigations.delete(key)
+        next unless code
+        navigation.instance_eval(&code)
+      end
+    end
   end
 end
