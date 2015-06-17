f6eeda80f597c90aa5d8eeeb5f9b5c52d3bed8ec
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-06-17 14:51:37 +0900

    Implement `image` method

--- lib/awesome_admin_layout/navigation/brand.rb
+++ lib/awesome_admin_layout/navigation/brand.rb
@@ -17,8 +17,12 @@ module AwesomeAdminLayout
         @item[:external_link] = link
       end
 
+      def image(src, option = {})
+        @item[:image] = option.merge(src: src)
+      end
+
       def to_s
-        %Q{<div class="#{__css_class}">#{__link_to("#{__name}")}#{__enternal_link}</div>}
+        %Q{<div class="#{__css_class}">#{__link_to("#{__image}#{__name}")}#{__enternal_link}</div>}
       end
 
       private
@@ -28,7 +32,7 @@ module AwesomeAdminLayout
       end
 
       def __name
-        "<span>#{@item[:name].is_a?(Proc) ? @item[:name].call : @item[:name]}</span>"
+        %Q{<span class="awesome_admin_layout-text">#{@item[:name].is_a?(Proc) ? @item[:name].call : @item[:name]}</span>}
       end
 
       def __link_to(name)
@@ -39,6 +43,18 @@ module AwesomeAdminLayout
       def __enternal_link
         return unless @item[:external_link]
         %Q{<a href="#{@item[:external_link]}" target="_blank" class="awesome_admin_layout-external-link">#{__external_link_icon}</a>}
+      end
+
+      def __image
+        image_tag = __image_tag
+        return unless image_tag
+        %Q{<span class="awesome_admin_layout-image">#{image_tag}</span>}
+      end
+
+      def __image_tag
+        return unless @item[:image]
+        return ActionController::Base.helpers.image_tag(@item[:image][:src], @item[:image]) if defined? Rails
+        %Q{<img src="#{@item[:image][:src]}" />}
       end
 
       def __external_link_icon
