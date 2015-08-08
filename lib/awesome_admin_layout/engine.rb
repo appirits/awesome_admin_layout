8a7bdf702490d1f2e8d93394943b45f6fb2549c9
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-08-08 23:49:20 +0900

    Add the loader for navigations

--- lib/awesome_admin_layout/engine.rb
+++ lib/awesome_admin_layout/engine.rb
@@ -7,5 +7,11 @@ module AwesomeAdminLayout
     ActiveSupport.on_load(:action_controller) do
       ActionController::Base.send(:extend, AwesomeAdminLayout::Helpers::ClassMethods)
     end
+
+    config.to_prepare do
+      Dir.glob(Rails.root.join('app/navigations/**/*.rb')).each do |c|
+        Rails.configuration.cache_classes ? require_dependency(c) : load(c)
+      end
+    end
   end
 end
